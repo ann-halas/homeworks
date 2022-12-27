9d280badee98375fa01691c5e6d7fd506cb50bec
Ann Halas <annagalas95@gmail.com>
2022-12-27 14:09:21 +0300

    Commit  hw10

--- src/s2-homeworks/hw05/pages/JuniorPlus.tsx
+++ src/s2-homeworks/hw05/pages/JuniorPlus.tsx
@@ -7,7 +7,7 @@ import HW13 from '../../hw13/HW13'
 function JuniorPlus() {
     return (
         <div id={'hw5-page-junior-plus'}>
-            {/*<HW10 />*/}
+            <HW10 />
             {/*<HW11 />*/}
             {/*<HW12 />*/}
             {/*<HW13 />*/}
